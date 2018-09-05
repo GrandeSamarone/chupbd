e7d40ea77867302d41aa4d09d3449e5a1b50183c
GrandeSamarone <30990831+GrandeSamarone@users.noreply.github.com>
2018-09-04 21:29:03 -0300

    Initial commit

---
+++ app/src/main/java/com/example/fulanoeciclano/nerdzone/autenticacao/LoginActivity.java
@@ -0,0 +1,149 @@
+package com.example.fulanoeciclano.nerdzone.autenticacao;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.fulanoeciclano.nerdzone.MainActivity;
+import com.example.fulanoeciclano.nerdzone.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 1;
+    private FirebaseAuth auth;
+    private GoogleSignInClient mGoogleSignInClient;
+    private FirebaseUser user;
+    private SignInButton botaologin;
+    private AlertDialog dialog;
+    private SharedPreferences sPreferences = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        auth = FirebaseAuth.getInstance();
+        user =auth.getCurrentUser();
+        botaologin = findViewById(R.id.sign_in_button);
+
+        botaologin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+
+        // SharedPreferences
+        sPreferences = getSharedPreferences("firstRun", MODE_PRIVATE);
+
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this,gso);
+
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+
+                // ...
+            }
+        }
+    }
+
+    public void Verificar(){
+
+        if (sPreferences.getBoolean("firstRun", true)) {
+            sPreferences.edit().putBoolean("firstRun", false).apply();
+            Intent it = new Intent(LoginActivity.this,teste.class);
+            startActivity(it);
+        } else {
+
+            Intent it = new Intent(LoginActivity.this,MainActivity.class);
+            startActivity(it);
+
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = auth.getCurrentUser();
+        if(currentUser!=null){
+            Verificar();
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+      //  Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Carregando...");
+        dialog = builder.create();
+        dialog.show();
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            dialog.dismiss();
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = auth.getCurrentUser();
+                            Intent it = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(it);
+                            finish();
+                            Toast.makeText(LoginActivity.this, "Sucesso", Toast.LENGTH_SHORT).show();
+                        } else {
+                            dialog.dismiss();
+                            Toast.makeText(LoginActivity.this, "Verifique a internet", Toast.LENGTH_SHORT).show();
+                            // If sign in fails, display a message to the user.
+                          /* Toast.makeText(GoogleSignInActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                           */
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/fulanoeciclano/nerdzone/autenticacao/teste.java
@@ -0,0 +1,15 @@
+package com.example.fulanoeciclano.nerdzone.autenticacao;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.fulanoeciclano.nerdzone.R;
+
+public class teste extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teste);
+    }
+}
